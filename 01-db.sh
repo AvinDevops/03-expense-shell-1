dae9fa4a14234e21df87dd7203f7b1f1c1a8d585
AvinPracticeDevOps <avinashkbandela@gmail.com>
2024-12-06 20:02:26 +0530

    calling another script

--- 01-db.sh
+++ 01-db.sh
@@ -2,6 +2,8 @@
 
 source ./00-common.sh
 
+echo "please enter DB password :"
+read mysql-root-password
 
 #Main code
 dnf install mysql-server -y &>>$LOGFILE
@@ -14,10 +16,10 @@ systemctl start mysqld &>>$LOGFILE
 CHECKSTATUS $? "Starting mysql-server"
 
 #mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-mysql -h db.avinexpense.online -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h db.avinexpense.online -uroot -p${mysql-root-password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql-root-password} &>>$LOGFILE
     CHECKSTATUS $? "Setting root password for mysql-server"
 else
     echo -e "$R Root password is already set $N"
